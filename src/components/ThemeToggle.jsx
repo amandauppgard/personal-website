fe10a257e3b3b9163e71a9e3757b39d5069d2dee
Amanda Uppgård <amanda.uppgard@abo.fi>
2025-12-06 14:49:36 +0200

    cleaned up css

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -6,10 +6,16 @@ import Moon from '@hackernoon/pixel-icon-library/icons/SVG/regular/moon.svg';
 export default function ThemeToggle() {
   const { theme, setTheme } = useContext(ThemeContext);
 
+  const updateTheme = () => {
+    document.startViewTransition(() => {
+      setTheme(theme === 'dark' ? 'light' : 'dark')
+    })
+  }
+
   return (
     <button
       className="theme-button"
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      onClick={() => updateTheme()}
     >
     {theme === "dark" ?
       <img className='theme-icon' src={Sun} alt="Toggle Theme" /> :
